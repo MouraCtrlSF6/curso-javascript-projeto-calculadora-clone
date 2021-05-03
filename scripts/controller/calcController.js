ca6c5ff3dcd230b7d5ed44f42a9458da8a2943d5
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-05-03 18:39:46 -0300

    Updating the project

---
+++ scripts/controller/calcController.js
@@ -0,0 +1,27 @@
+class calcController{
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+    initialize(){
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector("#date");
+        let timeEl = document.querySelector("#time");
+
+        dateEl.innerHTML = '03/05/2021';
+        timeEl.innerHTML = '18:39';
+    }
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    get currentDate(){
+        return this._currentDate;
+    }
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+    set currentDate(value){
+        this.currentDate = value;
+    }
+}
