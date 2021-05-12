faffe1a19790bb6571b83e7d7d413ee2d883ec9e
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-05-12 17:37:37 -0300

    Percent and double equal commands implemented

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -4,6 +4,9 @@ class calcController{
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._operation = [];
+        this._lastOperator = '+';
+        this._lastNumber = 0;
+        this._wasPercent;
         this.lang = "pt-BR";
         this.initialize(); 
         this.initButtonEvens(); 
@@ -50,9 +53,44 @@ class calcController{
                 this.displayCalc = '0';
         }
     }
+    MyScreen(showingMethod){
+        if(showingMethod){
+            for (let i = this._operation.indexOf(this.LastOperation); i > -1; i--){
+                if (!isNaN(this._operation[i])){
+                    this.displayCalc = this._operation[i];
+                    break;
+                }
+            }
+        }
+        else this.displayCalc = this._operation.join("");
+    }
     equalOperation(value){
-        if(this._operation.length > 3 || value == '='){
-            if (value === '%'){
+        if(this._operation.length == 1){
+            if (!this._wasPercent){
+                var myResult = eval(`${this._operation[0]} ${this._lastOperator} ${this._lastNumber}`);
+            }
+            else{
+                if (this._lastOperator == '+'){
+                    var myResult = this._operation[0]*this._lastNumber/100;
+                    myResult = this._operation[0] + myResult;
+                }
+                if (this._lastOperator == '-'){
+                    var myResult = this._operation[0]*this._lastNumber/100;
+                    myResult = this._operation[0] - myResult;
+                }
+                if (this._lastOperator == '*'){
+                    var myResult = this._operation[0]*this._lastNumber/100;
+                }
+                if (this._lastOperator == '/'){
+                    var myResult = this._operation[0]*this._lastNumber/100;
+                    myResult = this._operation[0] / myResult;
+                }
+            }
+            this._operation = [myResult];
+        }
+        else {
+            if (value === '%'){ 
+                this._wasPercent = true;
                 if (this._operation[1] == '+'){
                     var myResult = this._operation[0]*this._operation[2]/100;
                     myResult = this._operation[0] + myResult;
@@ -68,56 +106,46 @@ class calcController{
                     var myResult = this._operation[0]*this._operation[2]/100;
                     myResult = this._operation[0] / myResult;
                 }
-                this._operation = [];
-                this._operation[0] = myResult;
+                this._lastNumber = this._operation[2];
+                this._lastOperator = this._operation[1];
+                this._operation = [myResult];
             } 
             else{
+                this._wasPercent = false;
                 let myResult = eval(`${this._operation[0]} ${this._operation[1]} ${this._operation[2]}`);
-                this._operation = [];
-                this._operation[0] = myResult;
-                if (value !== '=') this._operation.push(value);
+                this._lastNumber = this._operation[2];
+                this._lastOperator = this._operation[1];
+                this._operation = [myResult];
+                console.log(this._lastOperator + ' ' + this._lastNumber);
+                if (value !== '=') this.pushOperation(value);
             }
-            this.displayCalc = this._operation.join("");
         }
+        this.MyScreen(false); //Doesn't matter
     }
     pushOperation(value){
         this._operation.push(value);
-        this.equalOperation(value);
+        if(this._operation.length > 3) this.equalOperation(value);
     }
     isOperator(value){
         return ['+', '-', '*', '/', '%'].indexOf(value) > -1;
     }
-    setLastOperation(value){
-        this._operation[this._operation.length - 1] = value; 
-    }
-    getLastOperation(){
-        return this._operation[this._operation.length - 1]; 
-    }
     addOperation(value){
-        if(isNaN(this.getLastOperation())){ 
+        if(isNaN(this.LastOperation)){ 
             if(this.isOperator(value)){ 
-                this.setLastOperation(value)
-                this.setMyScreen();
-            }
-            else if(isNaN(value)){ 
-
+                this.LastOperation = value;
             }
             else { 
                 this.pushOperation(value);
             }
         }
         else if(!isNaN(value)){ 
-                let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+            let newValue = this.LastOperation.toString() + value.toString();
+            this.LastOperation = parseInt(newValue);
         }
         else { 
             this.pushOperation(value);
         }
-        this.displayCalc = this._operation.join("");
-        //console.log(this._operation); 
-    }
-    setError(){
-        this.displayCalc = "Error";
+        this.MyScreen(false);
     }
     execBtn(value){
         switch(value){
@@ -143,7 +171,7 @@ class calcController{
                 this.addOperation('%');
                 break;
             case 'igual':
-                this.equalOperation('=');
+                if(this._operation.length >= 3 || this._operation.length == 1) this.equalOperation('=');
                 break;
             case '0':
             case '1':
@@ -158,7 +186,7 @@ class calcController{
                 this.addOperation(parseInt(value));
                 break;
             default:
-                this.setError();
+                this.displayCalc = "ERR";
         }
     }
     initButtonEvens(){
@@ -182,6 +210,9 @@ class calcController{
     get displayDate(){
         return new Date();
     }
+    get LastOperation(){
+        return this._operation[this._operation.length - 1]; 
+    }
     set displayCalc(value){
         this._displayCalcEl.innerHTML = value;
     }
@@ -190,5 +221,8 @@ class calcController{
     }
     set displayDate(value){
         this._dateEl.innerHTML = value;
+    }
+    set LastOperation(value){
+        this._operation[this._operation.length - 1] = value; 
     }
 }
